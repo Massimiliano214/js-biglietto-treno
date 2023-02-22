ae57252b24490eb9d37d944201ad21a1cfaabad7
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-02-22 17:43:09 +0100

    aggiunta risposta al NaN

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
-const km = parseInt(prompt("Inserisci il numero di Kilometri da percorrere"))
-const eta= parseInt(prompt("Inserisci età (possibilità sconti)"))
+const km = parseInt(prompt("Inserisci il numero di Kilometri da percorrere"));
+const eta= parseInt(prompt("Inserisci età (possibilità sconti)"));
 
 console.log(km);
 console.log(eta);
 
 
-let prezzo = km * 0.21 
+let prezzo = km * 0.21;
 
-console.log (prezzo)
+console.log (prezzo);
 
-if (eta > 65) {
-    prezzo = prezzo - ((prezzo * 40) / 100);
-} else if (eta < 18) {
-    prezzo = prezzo - ((prezzo * 20) / 100);
+if ((isNaN(km)) || (isNaN(eta))) {
+    document.getElementById("prezzoTreno").innerHTML = "Hai digitato incorrettamente";
+} else {
+    if (eta > 65) {
+        prezzo = prezzo - ((prezzo * 40) / 100);
+    } else if (eta < 18) {
+        prezzo = prezzo - ((prezzo * 20) / 100);
+    }
+    
+    console.log(prezzo);
+    console.log(prezzo.toFixed(2));
+    
+    if (eta > 65) {
+        document.getElementById("prezzoTreno").innerHTML = 
+        `
+            Hai uno sconto del 40%:
+            ${prezzo.toFixed(2)}
+            $
+        `;
+    } else if (eta < 18) {
+        document.getElementById("prezzoTreno").innerHTML = 
+        `
+            Hai uno sconto del 20%:
+            ${prezzo.toFixed(2)}
+            $
+        `;
+    } else {
+        document.getElementById("prezzoTreno").innerHTML = 
+        `
+            ${prezzo.toFixed(2)}
+            $
+        `;
+    }
 }
 
-console.log(prezzo)
-console.log(prezzo.toFixed(2))
 
-document.getElementById("prezzoTreno").innerHTML = 
-`
-    ${prezzo.toFixed(2)}
-    $
-`;
+
+
+
