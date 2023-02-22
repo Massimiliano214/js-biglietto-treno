26dffb0ca0b13c8a0ac7e134e8a1825f0986abba
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-02-22 14:58:26 +0100

    domande prompt

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const km = parseInt(prompt("Inserisci il numero di Kilometri da percorrere"))
+const eta= parseInt(prompt("Inserisci età (possibilità sconti)"))
+
+console.log(km);
+console.log(eta);
